76f80cd83d95e2276d0fc6e3936134eb1aec6251
Viachaslau Barysau <dirtyelegance8@gmail.com>
2020-10-19 22:12:22 +0300

    RET-14 Created Entities: InnerApplication, Item, Location, LocationItem. Added Enums: LocationType, Reason, Role.

---
+++ retailers/src/main/java/by/itechart/retailers/entity/Location.java
@@ -0,0 +1,47 @@
+package by.itechart.retailers.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "location")
+public class Location implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "identifier")
+    private String identifier;
+
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "customer_id")
+    private Customer customer;
+
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "address_id")
+    private Address address;
+
+    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "location_id")
+    private List<LocationItem> itemList;
+
+    @Column(name = "total_capacity")
+    private Integer totalCapacity;
+
+    @Column(name = "available_capacity")
+    private Integer availableCapacity;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "location_type")
+    private LocationType locationType;
+}
---
+++ retailers/src/main/java/by/itechart/retailers/entity/LocationType.java
@@ -0,0 +1,6 @@
+package by.itechart.retailers.entity;
+
+public enum LocationType {
+    WAREHOUSE,
+    SHOP
+}
