119ac73e68bae72c2e2d39a41f7e5ff9bad106d0
Kyrsor <egoraviov99@mail.ru>
2020-11-10 14:19:02 +0300

    RET-18, added logout

--- retailers/src/main/java/by/itechart/retailers/controller/AuthenticationController.java
+++ retailers/src/main/java/by/itechart/retailers/controller/AuthenticationController.java
@@ -5,19 +5,25 @@ import by.itechart.retailers.dto.UserDto;
 import by.itechart.retailers.security.jwt.JwtTokenProvider;
 import by.itechart.retailers.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.HashMap;
 import java.util.Map;
 
 @RestController
-@RequestMapping(value = "/api/v1/auth/")
+@RequestMapping(value = "")
 public class AuthenticationController {
 
     private final AuthenticationManager authenticationManager;
@@ -33,7 +39,7 @@ public class AuthenticationController {
         this.userService = userService;
     }
 
-    @PostMapping("login")
+    @PostMapping("/login")
     public ResponseEntity login(@RequestBody AuthenticationRequestDto requestDto) {
         try {
             String username = requestDto.getEmail();
@@ -49,10 +55,18 @@ public class AuthenticationController {
             Map<Object, Object> response = new HashMap<>();
             response.put("username", username);
             response.put("token", token);
+            response.put("roles", user.getUserRole());
 
             return ResponseEntity.ok(response);
         } catch (AuthenticationException e) {
-            throw new BadCredentialsException("Invalid username or password");
+            return new ResponseEntity<>("Invalid username or password", HttpStatus.FORBIDDEN);
+            //throw new BadCredentialsException("Invalid username or password");
         }
+    }
+
+    @PostMapping("/logout")
+    public void logout(HttpServletRequest request, HttpServletResponse response) {
+        SecurityContextLogoutHandler securityContextLogoutHandler = new SecurityContextLogoutHandler();
+        securityContextLogoutHandler.logout(request, response, null);
     }
 }
