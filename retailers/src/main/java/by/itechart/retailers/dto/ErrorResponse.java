6e3140a137e01b4f631dca18067fb097306ef310
Kyrsor <egoraviov99@mail.ru>
2020-11-17 21:58:05 +0300

    RET-19, if token expired, throws 401 Unauthorized

---
+++ retailers/src/main/java/by/itechart/retailers/dto/ErrorResponse.java
@@ -0,0 +1,11 @@
+package by.itechart.retailers.dto;
+
+import lombok.Data;
+import org.springframework.http.HttpStatus;
+
+@Data
+public class ErrorResponse {
+    HttpStatus httpStatus;
+    String message;
+
+}
