7551be6dbccd4b9c1cc7348b516af8056a188cb5
Kyrsor <egoraviov99@mail.ru>
2020-11-24 21:06:13 +0300

    RET-24, removed price in LocationProduct

--- retailers/src/main/java/by/itechart/retailers/entity/LocationProduct.java
+++ retailers/src/main/java/by/itechart/retailers/entity/LocationProduct.java
@@ -27,9 +27,6 @@ public class LocationProduct implements Serializable {
     @Column(name = "cost")
     private BigDecimal cost;
 
-    @Column(name = "price")
-    private BigDecimal price;
-
     @Column(name = "amount")
     private Integer amount;
 
