b9ee46c1fb411da02d20a5662732a3888df709d4
Kyrsor <egoraviov99@mail.ru>
2020-11-26 15:22:11 +0300

    RET-44, added sending congratulations on birthday and system admin notification when fails

--- retailers/src/main/java/by/itechart/retailers/RetailersApplication.java
+++ retailers/src/main/java/by/itechart/retailers/RetailersApplication.java
@@ -5,12 +5,16 @@ import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.retry.annotation.EnableRetry;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootConfiguration
 @EnableAutoConfiguration(exclude = {
         org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class
 })
 @ComponentScan
+@EnableRetry
+@EnableScheduling
 public class RetailersApplication {
 
     public static void main(String[] args) {
--- retailers/src/main/java/by/itechart/retailers/service/impl/SendingCredentialsServiceImpl.java
+++ retailers/src/main/java/by/itechart/retailers/service/impl/SendingCredentialsServiceImpl.java
@@ -2,20 +2,12 @@ package by.itechart.retailers.service.impl;
 
 import by.itechart.retailers.dto.UserDto;
 import by.itechart.retailers.service.interfaces.SendingCredentialsService;
+import by.itechart.retailers.service.interfaces.SendingService;
 import lombok.extern.slf4j.Slf4j;
 import org.antlr.stringtemplate.StringTemplate;
 import org.antlr.stringtemplate.StringTemplateGroup;
 import org.antlr.stringtemplate.language.DefaultTemplateLexer;
 import org.springframework.stereotype.Service;
-
-import javax.mail.Message;
-import javax.mail.MessagingException;
-import javax.mail.Session;
-import javax.mail.Transport;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
-import java.io.IOException;
-import java.util.Properties;
 
 @Slf4j
 @Service
@@ -30,31 +22,7 @@ public class SendingCredentialsServiceImpl implements SendingCredentialsService 
         mail.setAttribute("email", userDto.getEmail());
         mail.setAttribute("password", userDto.getPassword());
 
-        try {
-            Properties properties = new Properties();
-
-            properties.load(SendingCredentialsServiceImpl.class.getClassLoader()
-                                                               .getResourceAsStream("mail.properties"));
-
-            mail.setAttribute("retailersEmail", properties.get("mail.smtps.user")
-                                                          .toString());
-            Session session = Session.getDefaultInstance(properties);
-            MimeMessage message = new MimeMessage(session);
-            message.setFrom(new InternetAddress(properties.get("mail.smtps.user")
-                                                          .toString()));
-            message.addRecipient(Message.RecipientType.TO, new InternetAddress(userDto.getEmail()));
-            message.setSubject("Retailers Credentials");
-            message.setText(mail.toString());
-
-            Transport tr = session.getTransport();
-            tr.connect(properties.get("mail.smtps.user")
-                                 .toString(), properties.get("mail.smtps.password")
-                                                        .toString());
-            tr.sendMessage(message, message.getAllRecipients());
-            tr.close();
-        } catch (IOException | MessagingException e) {
-            log.error(e.getMessage());
-        }
+        SendingService.send(mail, userDto.getEmail());
 
     }
 }
