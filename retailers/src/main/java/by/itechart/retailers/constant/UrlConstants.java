ed3fe329a28c732f218c7b38b28a3a2390c3a861
Kyrsor <egoraviov99@mail.ru>
2020-12-09 14:17:01 +0300

    updated class of constants

---
+++ retailers/src/main/java/by/itechart/retailers/constant/UrlConstants.java
@@ -0,0 +1,19 @@
+package by.itechart.retailers.constant;
+
+public class UrlConstants {
+    public static final String URL_LOGIN_ENDPOINT = "/api/login";
+    public static final String URL_LOGOUT_ENDPOINT = "/api/logout";
+    public static final String URL_BILLS = "/api/bills/**";
+    public static final String URL_CATEGORIES = "/api/categories/**";
+    public static final String URL_CUSTOMERS = "/api/customers/**";
+    public static final String URL_INNER_APPLICATIONS = "/api/inner_applications/**";
+    public static final String URL_LOCATIONS = "/api/locations/**";
+    public static final String URL_LOCATION_PRODUCTS = "/api/location_products/**";
+    public static final String URL_PRODUCTS = "/api/products/**";
+    public static final String URL_STATES = "/api/states/**";
+    public static final String URL_SUPPLIER_APPLICATIONS = "/api/supplier_applications/**";
+    public static final String URL_SUPPLIERS = "/api/suppliers/**";
+    public static final String URL_SUPPLIER_WAREHOUSES = "/api/supplier_warehouses/**";
+    public static final String URL_USERS = "/api/users/**";
+    public static final String URL_WRITE_OFF_ACTS = "/api/write_of_acts/**";
+}
