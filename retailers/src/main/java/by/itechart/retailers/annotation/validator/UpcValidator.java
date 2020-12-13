da337a85b2968ba739e625be9981521a55f811af
Kyrsor <egoraviov99@mail.ru>
2020-12-13 14:23:51 +0300

    Refactoring

--- retailers/src/main/java/by/itechart/retailers/annotation/validator/UpcValidator.java
+++ retailers/src/main/java/by/itechart/retailers/annotation/validator/UpcValidator.java
@@ -11,35 +11,22 @@ public class UpcValidator implements ConstraintValidator<Upc, Long> {
         final long LARGEST_POSSIBLE_UPC_CODE = 999999999999L;
         long codeValue = upc;
         if ((codeValue > 0) && (codeValue < LARGEST_POSSIBLE_UPC_CODE)) {
-            // Get the individual digits of the UPC code, in reverse order
-            int n12 = (int) (codeValue % 10);
-            codeValue /= 10;
-            int n11 = (int) (codeValue % 10);
-            codeValue /= 10;
-            int n10 = (int) (codeValue % 10);
-            codeValue /= 10;
-            int n9 = (int) (codeValue % 10);
-            codeValue /= 10;
-            int n8 = (int) (codeValue % 10);
-            codeValue /= 10;
-            int n7 = (int) (codeValue % 10);
-            codeValue /= 10;
-            int n6 = (int) (codeValue % 10);
-            codeValue /= 10;
-            int n5 = (int) (codeValue % 10);
-            codeValue /= 10;
-            int n4 = (int) (codeValue % 10);
-            codeValue /= 10;
-            int n3 = (int) (codeValue % 10);
-            codeValue /= 10;
-            int n2 = (int) (codeValue % 10);
-            codeValue /= 10;
-            int n1 = (int) (codeValue % 10);
-           // codeValue /= 10;
-            int oddSum = n1 + n3 + n5 + n7 + n9 + n11;
-            int evenSum = n2 + n4 + n6 + n8 + n10;
-            int calcCheckDigit = (10 - ((evenSum + 3 * oddSum) % 10)) % 10;//здесь на гите поправка была
-            return calcCheckDigit == n12;
+            int[] codeValues = new int[12];
+            for (int i = codeValues.length - 1; i >= 0; i--) {
+                codeValues[i] = (int) (codeValue % 10);
+                codeValue /= 10;
+            }
+            int oddSum = 0;
+            int evenSum = 0;
+            for (int i = 0; i < codeValues.length; i += 2) {
+                oddSum += codeValues[i];
+            }
+            for (int i = 1; i < codeValues.length - 1; i += 2) {
+                evenSum += codeValues[i];
+            }
+            int calcCheckDigit = (10 - ((evenSum + 3 * oddSum) % 10)) % 10;
+            return calcCheckDigit == codeValues[11];
+
         } else {
             return false;
         }
